a2df16e4ab74d7d19608dbfe921b46b8e836fb2d
axdanbol <daniel_bolin@yahoo.com>
2020-11-04 13:02:29 -0500

    Added a placeholder test

--- website/projects/scimaps/src/app/app.component.spec.ts
+++ website/projects/scimaps/src/app/app.component.spec.ts
@@ -3,4 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  it('passes the placeholder test', () => {
+    expect(true).toBeTrue();
+  });
 });
