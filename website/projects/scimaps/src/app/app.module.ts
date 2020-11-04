fef155d16cfc4bd6d0f04b19e4fcebe0a2856437
axdanbol <daniel_bolin@yahoo.com>
2020-11-04 12:36:50 -0500

    Added angular material w/ theming (WIP)

--- website/projects/scimaps/src/app/app.module.ts
+++ website/projects/scimaps/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,9 +10,10 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
 
     AppRoutingModule,
-    CoreModule
+    CoreModule,
   ],
   declarations: [AppComponent],
   providers: [],
