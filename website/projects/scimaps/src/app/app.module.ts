1426c7b7d5cb24889e988025c6b8358a63ffa287
axdanbol <daniel_bolin@yahoo.com>
2020-11-04 12:26:15 -0500

    Added global state

--- website/projects/scimaps/src/app/app.module.ts
+++ website/projects/scimaps/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    AppRoutingModule,
+    CoreModule
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
