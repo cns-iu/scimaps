93dca3b47cbbc63b881528fa2a16669265940229
axdanbol <daniel_bolin@yahoo.com>
2020-11-10 09:56:49 -0500

    Footer component implementation

--- website/projects/scimaps/src/app/core/components/footer/footer.component.ts
+++ website/projects/scimaps/src/app/core/components/footer/footer.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 
 
+/**
+ * App footer
+ */
 @Component({
   selector: 'sci-footer',
   templateUrl: './footer.component.html',
@@ -8,4 +11,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
+  /** HTML class */
+  @HostBinding('class') readonly clsName = 'sci-footer';
+
+  /** Phone number - formatted! */
+  @Input() phoneNumber!: string;
+
+  /** Acknowledgement text */
+  @Input() acknowledgement!: string;
 }
--- website/projects/scimaps/src/app/core/components/footer/footer.module.ts
+++ website/projects/scimaps/src/app/core/components/footer/footer.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { FooterComponent } from './footer.component';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [FooterComponent],
   exports: [FooterComponent],
