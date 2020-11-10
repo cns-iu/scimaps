866564d4cdb2e01cb0b55eb67c91c66fa6aff3f5
axdanbol <daniel_bolin@yahoo.com>
2020-11-10 09:55:56 -0500

    Load icons in app module

--- website/projects/scimaps/src/app/app.module.ts
+++ website/projects/scimaps/src/app/app.module.ts
@@ -1,22 +1,41 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
+    MatIconModule,
 
     AppRoutingModule,
     CoreModule,
+    SharedModule,
   ],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(registry: MatIconRegistry, sanitizer: DomSanitizer) {
+    const icons = [
+      { namespace: 'social', name: 'twitter', url: 'assets/social-media-logos/twitter.svg' }
+    ];
+
+    for (const icon of icons) {
+      const { namespace, name, url: rawUrl } = icon;
+      const url = sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+      registry.addSvgIconInNamespace(namespace, name, url);
+    }
+  }
+}
