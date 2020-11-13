36f241ef99fb22623c3b5e7f50d98961ab5e99c8
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-11-13 16:03:13 -0500

    Documentation

--- website/projects/scimaps/src/app/shared/components/news-item-list/news-item-list.component.ts
+++ website/projects/scimaps/src/app/shared/components/news-item-list/news-item-list.component.ts
@@ -4,31 +4,66 @@ import { MatSelectChange } from '@angular/material/select';
 import { NewsItem } from '../news-item/news-item.model';
 
 
+/**
+ * Component for displaying news items
+ */
 @Component({
   selector: 'sci-news-item-list',
   templateUrl: './news-item-list.component.html',
   styleUrls: ['./news-item-list.component.scss']
 })
 export class NewsItemListComponent implements OnInit {
-
+  /**
+   * Component class name
+   */
   @HostBinding('class') readonly clsName = 'sci-news-item-list';
 
+  /**
+   * All news items
+   */
   @Input() newsItems!: NewsItem[];
 
+  /**
+   * News items to be displayed
+   */
   @Input() displayedNewsItems: NewsItem[] = this.newsItems;
 
+  /**
+   * Order for date sorting (ascending or descending)
+   */
   dateOrder = 'asc';
+
+  /**
+   * Order for title sorting (ascending or descending)
+   */
   titleOrder = 'asc';
+
+  /**
+   * Order for publication sorting (ascending or descending)
+   */
   publicationOrder = 'asc';
 
+  /**
+   * Whether all items should be displayed
+   */
   showAllItems = false;
 
+  /**
+   * Currently selected year
+   */
   selectedYear = 'All';
 
+  /**
+   * Sorts all items by date on init
+   */
   ngOnInit(): void {
     this.sort('date');
   }
 
+  /**
+   * Sorts displayed news items according to criteria
+   * @param criteria Criteria to be sorted by
+   */
   sort(criteria: 'date' | 'publication' | 'title'): void {
     let order = 'asc';
     if (criteria === 'publication') {
@@ -44,6 +79,12 @@ export class NewsItemListComponent implements OnInit {
     this.displayedNewsItems = [...this.displayedNewsItems].sort(this.compareValues(criteria, order));
   }
 
+  /**
+   * Comparison function for NewsItems
+   * @param key Criteria being compared
+   * @param order order to compare
+   * @returns 1 if a[key] is greater than b[key], -1 if less (opposite for descending). 0 if equal.
+   */
   compareValues(key: 'date' | 'publication' | 'title', order = 'asc'): (a: NewsItem, b: NewsItem) => number {
     return (a, b) => {
       if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
@@ -63,23 +104,37 @@ export class NewsItemListComponent implements OnInit {
     };
   }
 
+  /**
+   * List of options to be displayed in year select dropdown
+   */
   get yearList(): string[] {
     const years = this.newsItems.map(item => item.date.getFullYear().toString());
     return ['All'].concat([...new Set(years)]);
   }
 
-  onYearChange(event: MatSelectChange): void {
-    if (event.value === 'All') {
+  /**
+   * Filters displayed news items by selected year
+   * @param value Selected value
+   */
+  onYearChange(value: string): void {
+    if (value === 'All') {
       this.displayedNewsItems = [...this.newsItems];
     } else {
-      this.displayedNewsItems = [...this.newsItems].filter((item) => item.date.getFullYear().toString() === event.value);
+      this.displayedNewsItems = [...this.newsItems].filter((item) => item.date.getFullYear().toString() === value);
     }
   }
 
+  /**
+   * Text to be displayed in show more button
+   */
   get showButtonText(): string {
     return (this.showAllItems) ? 'Show Less' : 'Show More';
   }
 
+  /**
+   * Determines if show more button should be shown
+   * @returns true if there are 4 or fewer items displayed
+   */
   needShowMoreButton(): boolean {
     return (this.displayedNewsItems.length < 5) ? false : true;
   }
