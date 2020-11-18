b2c329e7f025c137a60c9c5a59e39d6137bcef8d
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-11-18 17:04:31 -0500

    Adjust styling of image gallery

--- website/projects/scimaps/src/app/pages/maps-page/maps-page.component.ts
+++ website/projects/scimaps/src/app/pages/maps-page/maps-page.component.ts
@@ -15,144 +15,244 @@ export class MapsPageComponent implements OnInit {
   highlightBody = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo. Pellentesque ultrices blandit diam quis pretium. Suspendisse ut ante in enim consequat semper id consectetur arcu. Proin ultricies vestibulum nulla sit amet.';
   discoverItems: DiscoverItem[] = [
     {
-      title: 'XV: macroscopes for Tracking the Flow of Resources (2019)',
+      title: 'X. The Future of Science Mapping',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo.',
       thumbnails: [
         {
-          title: 'Refugee Flow',
+          title: '',
           link: 'www.google.com',
-          image: 'image1.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Politoscope',
+          title: '',
           link: 'www.google.com',
-          image: 'image2.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Income Disparity',
+          title: '',
           link: 'www.google.com',
-          image: 'image3.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Making Sense of Skills',
+          title: '',
           link: 'www.google.com',
-          image: 'image4.png'
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
         }
       ],
-      slug: 'macroscopes-2019',
-      directory: 'macroscopes'
+      slug: 'maps-2019',
+      directory: 'maps'
     },
     {
-      title: 'XV: macroscopes for Tracking the Flow of Resources (2019)',
+      title: 'VIII. Science Maps Showing Trends & Dynamics (2013)',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo.',
       thumbnails: [
         {
-          title: 'Refugee Flow',
+          title: '',
           link: 'www.google.com',
-          image: 'image1.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Politoscope',
+          title: '',
           link: 'www.google.com',
-          image: 'image2.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Income Disparity',
+          title: '',
           link: 'www.google.com',
-          image: 'image3.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Making Sense of Skills',
+          title: '',
           link: 'www.google.com',
-          image: 'image4.png'
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
         }
       ],
-      slug: 'macroscopes-2019',
-      directory: 'macroscopes'
+      slug: 'maps-2019',
+      directory: 'maps'
     },
     {
-      title: 'XV: macroscopes for Tracking the Flow of Resources (2019)',
+      title: 'VIII. Science Maps for Kids (2012)',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo.',
       thumbnails: [
         {
-          title: 'Refugee Flow',
+          title: '',
           link: 'www.google.com',
-          image: 'image1.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Politoscope',
+          title: '',
           link: 'www.google.com',
-          image: 'image2.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Income Disparity',
+          title: '',
           link: 'www.google.com',
-          image: 'image3.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Making Sense of Skills',
+          title: '',
           link: 'www.google.com',
-          image: 'image4.png'
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
         }
       ],
-      slug: 'macroscopes-2019',
-      directory: 'macroscopes'
+      slug: 'maps-2019',
+      directory: 'maps'
     },
     {
-      title: 'XV: macroscopes for Tracking the Flow of Resources (2019)',
+      title: 'VII.',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo.',
       thumbnails: [
         {
-          title: 'Refugee Flow',
+          title: '',
           link: 'www.google.com',
-          image: 'image1.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Politoscope',
+          title: '',
           link: 'www.google.com',
-          image: 'image2.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Income Disparity',
+          title: '',
           link: 'www.google.com',
-          image: 'image3.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Making Sense of Skills',
+          title: '',
           link: 'www.google.com',
-          image: 'image4.png'
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
         }
       ],
-      slug: 'macroscopes-2019',
-      directory: 'macroscopes'
+      slug: 'maps-2019',
+      directory: 'maps'
     },
     {
-      title: 'XV: macroscopes for Tracking the Flow of Resources (2019)',
+      title: 'VI.',
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo.',
       thumbnails: [
         {
-          title: 'Refugee Flow',
+          title: '',
           link: 'www.google.com',
-          image: 'image1.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Politoscope',
+          title: '',
           link: 'www.google.com',
-          image: 'image2.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Income Disparity',
+          title: '',
           link: 'www.google.com',
-          image: 'image3.png'
+          image: 'rose.jpg'
         },
         {
-          title: 'Making Sense of Skills',
+          title: '',
           link: 'www.google.com',
-          image: 'image4.png'
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
+        },
+        {
+          title: '',
+          link: 'www.google.com',
+          image: 'rose.jpg'
         }
       ],
-      slug: 'macroscopes-2019',
-      directory: 'macroscopes'
+      slug: 'maps-2019',
+      directory: 'maps'
     }
   ];
   displayItems: DiscoverItem[] = [];
