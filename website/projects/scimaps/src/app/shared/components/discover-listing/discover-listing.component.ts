cc2947fe5db7ee615ffc9dc033173d339c922623
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-11-18 13:48:22 -0500

    simplified mobile vs. desktop

--- website/projects/scimaps/src/app/shared/components/discover-listing/discover-listing.component.ts
+++ website/projects/scimaps/src/app/shared/components/discover-listing/discover-listing.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+
 import { DiscoverItem, ThumbnailLink } from '../../../core/models/discover-item';
 import { WarningDialogComponent } from '../warning-dialog/warning-dialog.component';
 
@@ -8,41 +9,30 @@ import { WarningDialogComponent } from '../warning-dialog/warning-dialog.compone
   templateUrl: './discover-listing.component.html',
   styleUrls: ['./discover-listing.component.scss']
 })
-export class DiscoverListingComponent implements OnInit {
+export class DiscoverListingComponent {
   /** HTML class name */
   @HostBinding('class') readonly clsName = 'sci-discover-listing';
 
   @Input() discoverItem!: DiscoverItem;
-  public screenWidth = 0;
-  mobileScreenSize = 640;
   mobileWarning = 'Macroscopes work best on desktop or larger tablet screens.  You may have a less than optimal experience on this device.';
 
   constructor(private readonly dialog: MatDialog) { }
-
-  ngOnInit(): void {
-    this.screenWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.screenWidth = window.innerWidth;
-  }
 
   imageSource(image: string): string {
     return `assets/${this.discoverItem.directory}/${this.discoverItem.slug}/${image}`;
   }
 
-  thumbnailClickHandler(thumbnail: ThumbnailLink): void {
-    if (this.screenWidth <= this.mobileScreenSize) {
-      this.dialog.open(WarningDialogComponent, {
-        width: '95%',
-        data: {
-          warningMessage: this.mobileWarning,
-          closeLink: thumbnail.link
-        }
-      });
-    } else {
-      window.open(thumbnail.link, '_blank');
-    }
+  mobileThumbnailClickHandler(thumbnail: ThumbnailLink): void {
+    this.dialog.open(WarningDialogComponent, {
+      width: '95%',
+      data: {
+        warningMessage: this.mobileWarning,
+        closeLink: thumbnail.link
+      }
+    });
+  }
+
+  desktopThumbnailClickHandler(thumbnail: ThumbnailLink): void {
+    window.open(thumbnail.link, '_blank');
   }
 }
