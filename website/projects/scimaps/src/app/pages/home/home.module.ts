17393c2592adb102130385ac4e5c907345be5d45
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-12-03 14:33:15 -0500

    social sidebar component created, svg icons added

--- website/projects/scimaps/src/app/pages/home/home.module.ts
+++ website/projects/scimaps/src/app/pages/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { SocialSidebarModule } from '../../shared/components/social-sidebar/social-sidebar.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SocialSidebarModule
   ],
   declarations: [HomeComponent]
 })
