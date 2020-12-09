39d15b4945fef8dbde2f166a839c54b7942adc74
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-12-09 16:10:10 -0500

    tests and theming

--- website/projects/scimaps/src/app/shared/components/lazy-cards/lazy-cards.component.spec.ts
+++ website/projects/scimaps/src/app/shared/components/lazy-cards/lazy-cards.component.spec.ts
@@ -1,25 +1,70 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Shallow } from 'shallow-render';
+import { Venue } from '../../../core/models/venue';
 
 import { LazyCardsComponent } from './lazy-cards.component';
+import { LazyCardsModule } from './lazy-cards.module';
+
+function getVenues(count: number): Venue[] {
+  const venues: Venue[] = [];
+  const sampleVenue: Venue = {
+    startDate: '9/2/2020',
+    endDate: '9/30/2020',
+    event: 'Technology Petting Zoo',
+    location: 'Indiana University UITS, Bloomington, IN',
+    contact: 'Jeannette Lehr',
+    photoLink: 'www.google.com',
+    videoLink: 'www.google.com'
+  };
+
+  for (let i = 0; i < count; i++) {
+    venues.push(sampleVenue);
+  }
+
+  return venues;
+}
 
 describe('LazyCardsComponent', () => {
-  let component: LazyCardsComponent;
-  let fixture: ComponentFixture<LazyCardsComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LazyCardsComponent ]
-    })
-    .compileComponents();
-  });
+  let shallow: Shallow<LazyCardsComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LazyCardsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    shallow = new Shallow(LazyCardsComponent, LazyCardsModule);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('displayItems should be the same length as itemsToDisplay when there are >= number of items', async () => {
+    const data = getVenues(3);
+    const { instance } = await shallow.render({ bind: { data }});
+    instance.itemsToDisplay = 2;
+    expect(instance.displayItems.length).toEqual(instance.itemsToDisplay);
   });
+
+  it('moreItemsToDisplay should be true when there are more items than itemsToDisplay', async () => {
+    const data = getVenues(4);
+    const { instance } = await shallow.render({ bind: { data }});
+    instance.itemsToDisplay = 3;
+    expect(instance.moreItemsToDisplay).toBeTrue();
+  });
+
+  it('moreItemsToDisplay should be false when there are not more items than itemsToDisplay', async () => {
+    const data = getVenues(2);
+    const { instance } = await shallow.render({ bind: { data }});
+    instance.itemsToDisplay = 2;
+    expect(instance.moreItemsToDisplay).toBeFalse();
+  });
+
+  it('should call window.open with the correct link when goToLink is called', async () => {
+    const data = getVenues(1);
+    const { instance } = await shallow.render({ bind: { data }});
+    const spy = spyOn(window, 'open');
+    instance.goToLink('www.test.com');
+    expect(spy).toHaveBeenCalledWith('www.test.com', '_blank');
+  });
+
+  it('should incrememnet itemsToDisplay by ITEMS_TO_DISPLAY_INCREMEMENT when showMore is called', async () => {
+    const data = getVenues(1);
+    const { instance } = await shallow.render({ bind: { data }});
+    const before = instance.itemsToDisplay;
+    instance.showMore();
+    const after = instance.itemsToDisplay;
+    expect(after).toEqual(before + instance.ITEMS_TO_DISPLAY_INCREMENT);
+  })
 });
