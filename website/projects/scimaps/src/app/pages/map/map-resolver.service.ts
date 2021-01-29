b0e4a479aaa44b6e448bb61ec307ad4b066ae09c
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-01-29 17:08:56 -0500

    Correct formatting for maker names

--- website/projects/scimaps/src/app/pages/map/map-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/map/map-resolver.service.ts
@@ -3,7 +3,7 @@ import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@a
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
-import { DiscoverItem, MapItem } from '../../core/models/discover-item';
+import { MapItem } from '../../core/models/discover-item';
 import { ContentService } from '../../shared/services/content.service';
 
 
@@ -12,20 +12,28 @@ import { ContentService } from '../../shared/services/content.service';
 })
 export class MapResolverService implements Resolve<MapItem> {
 
-  constructor(private content: ContentService, private router: Router) { }
+  constructor(private content: ContentService) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<MapItem> | Observable<never> {
+  resolve(route: ActivatedRouteSnapshot): Observable<MapItem> | Observable<never> {
+    const language = 'en'; //how to change this?
     const iteration = route.paramMap.get('iteration');
     const sequence = route.paramMap.get('sequence');
     const mapSlug = `map/${iteration}/${sequence}`;
     return this.content.getContent(mapSlug).pipe(take(1), map<any, MapItem>((data) => {
       const item: MapItem = {} as MapItem;
-      item.title = data.en.title;
-      item.makers = data.en.makers.map((m: any) => m.replace('/readme', '').replace('-', ' ')),
-      item.credit = 'Credit line text';
-      item.description = data.en.body;
+      item.title = data[language].title;
+      item.makers = data[language].makers.map((m: any) => {
+        m = m.replace('/readme', '').replace('-', ' ');
+        let names = m.split(' ')
+        for (let i = 0, x = names.length; i < x; i++) {
+          names[i] = names[i][0].toUpperCase() + names[i].substr(1);
+        }
+        return names.join(' ')
+      });
+      item.credit = data[language].creditLine;
+      item.description = data[language].body;
+      item.references = data[language].references;
       item.thumbnail = `assets/content/map/${data.en.iteration}/${data.en.sequence}/${data.en.image.lg}`;
-      item.references = data.en.references;
       return item;
     }));
   }
--- website/projects/scimaps/src/app/pages/map/map.component.ts
+++ website/projects/scimaps/src/app/pages/map/map.component.ts
@@ -1,9 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { take } from 'rxjs/operators';
-import { ContentService } from '../../shared/services/content.service';
 
-import { DiscoverItem, MapItem } from '../../core/models/discover-item';
+import { MapItem } from '../../core/models/discover-item';
 
 @Component({
   selector: 'sci-map',
@@ -11,36 +9,20 @@ import { DiscoverItem, MapItem } from '../../core/models/discover-item';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  
-  discoverItems!: DiscoverItem[];
 
-  displayItems: DiscoverItem[] = [];
+  selectedItem!: MapItem;
 
-  selectedItem: MapItem = {
-    title: 'Sample Map Item',
-    makers: ['Homer Simpson', 'Elon Musk'],
-    location: 'Springfield',
-    credit: 'Credit line text',
-    year: '2000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non dui euismod mauris faucibus euismod non lacinia quam. Morbi sit amet placerat dui. Sed ut dolor efficitur, consequat augue sed, pharetra orci. Nulla vitae mauris nisi. Aenean orci ipsum, scelerisque et arcu quis, molestie efficitur dui. Curabitur enim lacus, vehicula at arcu id, sagittis posuere est. Maecenas laoreet est eget tristique interdum. Fusce consequat, nisl ac bibendum facilisis, tellus nulla blandit orci, quis dignissim est mi ac justo. Pellentesque ultrices blandit diam quis pretium. Suspendisse ut ante in enim consequat semper id consectetur arcu. Proin ultricies vestibulum nulla sit amet.',
-    references: [
-      'Reference 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        'Fusce non dui euismod mauris faucibus euismod non lacinia quam.',
-      'Reference 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        'Fusce non dui euismod mauris faucibus euismod non lacinia quam.',
-      'Reference 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-        'Fusce non dui euismod mauris faucibus euismod non lacinia quam.'
-    ],
-    thumbnail: 'assets/maps/maps-2019/rose.jpg'
-  };;
+  currentLanguage = 'en';
 
-
-  constructor(private route: ActivatedRoute, private content: ContentService) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.data.subscribe((data) => {
-      // this.discoverItems = data.maps;
       this.selectedItem = data.map;
     });
+  }
+
+  changeLanguage(language: string): void {
+    this.currentLanguage = language;
   }
 }
