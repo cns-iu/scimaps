2b7f6d907c497a27c42ca4f82e22cb6978b372ea
swapnil <ddevil691@gmail.com>
2021-05-07 10:37:56 -0400

    pbi-about-page removed comments

--- website/projects/scimaps/src/app/pages/about/about.component.ts
+++ website/projects/scimaps/src/app/pages/about/about.component.ts
@@ -13,9 +13,9 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // console.log(this.route);
     this.route.data.subscribe(data => {
       const about = data.about
+      // Temporary code
       this.curatorProfiles = about.slice(0, 3)
       this.advisoryBoardProfiles = about.slice(3, 12)
       this.ambassadorProfiles = about.slice(12, 21)
@@ -28,106 +28,6 @@ export class AboutComponent implements OnInit {
   curatorProfiles = [];
   advisoryBoardProfiles = [];
   ambassadorProfiles = []
-  // placeholderProfilesShort: Profile[] = [
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   }
-  // ];
-  // placeholderProfilesLong: Profile[] = [
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   },
-  //   {
-  //     name: 'Name Surname',
-  //     title: 'Job Title',
-  //     affiliation: 'Affiliation',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum, massa nec elementum convallis, ligula mi placerat ipsum, et volutpat ante orci ultricies odio. Duis.',
-  //     link: 'www.google.com',
-  //     slug: 'name-surname'
-  //   }
-  // ];
   
   placeholderDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam ligula felis, id blandit quam viverra auctor. Nullam quam dolor, mattis consequat tincidunt sed, condimentum id sapien. Pellentesque ut congue lectus. Sed leo velit, rutrum vitae sapien sed, faucibus tincidunt justo. Donec et vestibulum enim, a efficitur purus. Mauris quis lacinia nibh, eget suscipit felis. Suspendisse ut elementum metus, ac sagittis sem. ';
   pageTabs = ['Curators/Advisory Board', 'Ambassadors'];
