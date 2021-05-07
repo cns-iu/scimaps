671fdc3ae9cdd944da5c1603cb95f535987bb350
swapnil <ddevil691@gmail.com>
2021-05-07 10:21:27 -0400

    pbi-about-page Added mapping logic in resolver

--- website/projects/scimaps/src/app/pages/about/about-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/about/about-resolver.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { ContentService } from '../../shared/services/content.service';
-import { take } from 'rxjs/operators';
+import { ContentService, toSlug } from '../../shared/services/content.service';
+import { take, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Profile } from '../../core/models/profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutResolverService implements Resolve<{[key: string]: any}[]> {
 
-  // TODO: Model for return type
   constructor(private content: ContentService) { }
 
-  resolve(): Observable<{[key: string]: any}[]> | Observable<never> {
-    console.log('resolve call')
-    return this.content.getIndex<{[key: string]: any}>('people').pipe(take(1));
+  resolve(): Observable<Profile[]> | Observable<never> {
+    return this.content.getIndex<{[key: string]: string}>('people').pipe(
+      take(1),
+      map((items: any[]) => {
+        return items.map((item: any) => {
+            const profile: Profile = {
+            name: item.name,
+            slug: toSlug(item.name),
+            title: 'Title',
+            link: 'https://www.google.com',
+            body: 'Body',
+            affiliation: 'Affiliation',
+            image: item.image
+          }
+          return profile;
+        });
+      }),
+    )
   }
 }
