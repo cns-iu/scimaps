95c785c369dded0819faf826dd13e9c062c09a50
swapnil <ddevil691@gmail.com>
2021-05-10 13:41:56 -0400

    abi-about-page  populated description on UI

--- website/projects/scimaps/src/app/pages/about/about.component.ts
+++ website/projects/scimaps/src/app/pages/about/about.component.ts
@@ -15,19 +15,24 @@ export class AboutComponent implements OnInit {
   @HostBinding('class') readonly clsName = 'sci-about';
 
   curatorProfiles = [];
+  curatorsDescription = '';
   advisoryBoardProfiles = [];
+  advisoryBoardDescription = '';
   ambassadorProfiles = [];
-  placeholderDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam ligula felis, id blandit quam viverra auctor. Nullam quam dolor, mattis consequat tincidunt sed, condimentum id sapien. Pellentesque ut congue lectus. Sed leo velit, rutrum vitae sapien sed, faucibus tincidunt justo. Donec et vestibulum enim, a efficitur purus. Mauris quis lacinia nibh, eget suscipit felis. Suspendisse ut elementum metus, ac sagittis sem. ';
+  ambassadorsDescription = '';
   pageTabs = ['Curators/Advisory Board', 'Ambassadors'];
   activePageTab = 0;
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
-      const about = data.about;
+      const {profiles, body} = data;
       // Temporary code. Later will be replaced with actual filtering logic.
-      this.curatorProfiles = about.slice(0, 3);
-      this.advisoryBoardProfiles = about.slice(3, 12);
-      this.ambassadorProfiles = about.slice(12, 21);
+      this.curatorProfiles = profiles.slice(0, 3);
+      this.curatorsDescription = body.curatorsDescription;
+      this.advisoryBoardProfiles = profiles.slice(3, 12);
+      this.advisoryBoardDescription = body.advisoryBoardDescription;
+      this.ambassadorProfiles = profiles.slice(12, 21);
+      this.ambassadorsDescription = body.ambassadorsDescription;
     });
   }
 
