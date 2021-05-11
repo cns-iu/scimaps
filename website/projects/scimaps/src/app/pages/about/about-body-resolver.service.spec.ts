b1a4091edca902d67c0c6af27fdcb86621ebebe7
swapnil <ddevil691@gmail.com>
2021-05-11 14:20:08 -0400

    pbi-about-page removed empty line

--- website/projects/scimaps/src/app/pages/about/about-body-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/about/about-body-resolver.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AboutBodyResolverService } from './about-body-resolver.service';
 import { ContentService } from '../../shared/services/content.service';
 
