bcf90d3ec5c789148707bf1c7f833d262f2c9e34
swapnil <ddevil691@gmail.com>
2021-05-11 15:36:09 -0400

    pbi-about-page Added specs for resolvers

--- website/projects/scimaps/src/app/pages/about/about-body-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/about/about-body-resolver.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AboutBodyResolverService } from './about-body-resolver.service';
 import { ContentService } from '../../shared/services/content.service';
+import { of } from 'rxjs';
+import { Params } from '@angular/router';
 
 describe('AboutBodyResolverService', () => {
   let service: AboutBodyResolverService;
-  let contentServiceSpy: ContentService;
+  let contentService: Params;
   beforeEach(() => {
-    contentServiceSpy = jasmine.createSpyObj(ContentService, ['getContent']);
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', ['getContent']);
     TestBed.configureTestingModule({
       providers: [
         AboutBodyResolverService,
@@ -14,9 +16,25 @@ describe('AboutBodyResolverService', () => {
       ]
     });
     service = TestBed.inject(AboutBodyResolverService);
+    contentService = TestBed.inject(ContentService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call contentService.getContent once', () => {
+    (contentService.getContent as jasmine.Spy).and.returnValue(
+      of({
+        body: {
+          curatorsDescription: 'Sample curator description',
+          advisoryBoardDescription: 'Sample advisory board description',
+          ambassadorsDescription: 'Sample ambassadors description'
+        }
+      })
+    );
+    service.resolve();
+    expect(contentService.getContent).toHaveBeenCalledTimes(1);
+    expect(contentService.getContent).toHaveBeenCalledWith('site/about.md');
   });
 });
--- website/projects/scimaps/src/app/pages/about/about-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/about/about-resolver.service.spec.ts
@@ -2,15 +2,25 @@ import { TestBed } from '@angular/core/testing';
 import { AboutResolverService } from './about-resolver.service';
 import { Profile } from '../../core/models/profile';
 import { getProfiles } from './about.component.spec';
-import { ContentService } from '../../shared/services/content.service';
+import { ContentService, toSlug } from '../../shared/services/content.service';
+import { Params } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AboutResolverService', () => {
   let service: AboutResolverService;
-  let contentServiceSpy: ContentService;
+  let contentService: Params;
   const testProfile: Profile[] = getProfiles(10);
-
+  const directory = 'content/person';
+  const testItem = {
+    name: 'Achim Oberg',
+    jobTitle: 'jobtitle',
+    homepage: 'homepage',
+    body: 'body',
+    affiliation: 'affiliation',
+    image: 'image.png'
+  };
   beforeEach(() => {
-    contentServiceSpy = jasmine.createSpyObj(ContentService, ['getIndex']);
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', ['getIndex']);
     TestBed.configureTestingModule({
       providers: [
         AboutResolverService,
@@ -18,6 +28,7 @@ describe('AboutResolverService', () => {
       ]
     });
     service = TestBed.inject(AboutResolverService);
+    contentService = TestBed.inject(ContentService);
   });
 
   it('should be created', () => {
@@ -26,8 +37,33 @@ describe('AboutResolverService', () => {
 
   it('should give correct relative image path', () => {
     const profile = testProfile[0];
-    const directory = 'content/person';
     const newImagePath = service.getImageSource(profile);
     expect(newImagePath).toEqual(`assets/${directory}/${profile.slug}/${profile.image}`);
+  });
+
+  it('should should call contentService.getIndex once', () => {
+    (contentService.getIndex as jasmine.Spy).and.returnValue(
+      of([testItem])
+    );
+    service.resolve();
+    expect(contentService.getIndex).toHaveBeenCalledTimes(1);
+    expect(contentService.getIndex).toHaveBeenCalledWith('people');
+  });
+
+  it('should map data correctly', async (done) => {
+    (contentService.getIndex as jasmine.Spy).and.returnValue(
+      of([testItem])
+    );
+    const result = service.resolve();
+    result.subscribe((profiles: Profile[]) => {
+      const profile = profiles[0];
+      expect(profile.name).toEqual(testItem.name);
+      expect(profile.body).toEqual(testItem.body);
+      expect(profile.affiliation).toEqual(testItem.affiliation);
+      expect(profile.link).toEqual(testItem.homepage);
+      expect(profile.slug).toEqual(toSlug(testItem.name));
+      expect(profile.image).toEqual(`assets/${directory}/${profile.slug}/${testItem.image}`);
+      done();
+    });
   });
 });
