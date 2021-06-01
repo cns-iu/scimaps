7a34c6ddebdc7ab675055eb65e07866c4256e292
swapnil <ddevil691@gmail.com>
2021-06-01 15:02:06 -0400

    pbi-books added reactive forms and Ui for filter

--- website/projects/scimaps/src/app/shared/components/data-table/data-table.component.ts
+++ website/projects/scimaps/src/app/shared/components/data-table/data-table.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { switchMap } from 'rxjs/operators';
 import { NewsItem } from '../news-item/news-item.model';
 
 @Component({
@@ -11,26 +14,40 @@ import { NewsItem } from '../news-item/news-item.model';
 export class DataTableComponent implements OnInit, AfterViewInit {
   @Input() data: NewsItem[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   dataSource = new MatTableDataSource(this.data);
   yearList = ['2021', '2020']
   selectedYear = '2021';
   
-  constructor() { }
+  searchForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.dataSource.data = this.data;
+    this.searchForm = this.formBuilder.group({
+      year: this.formBuilder.control(''),
+      search: this.formBuilder.control('')
+    });
+
+    // this.searchForm.get('year')?.valueChanges.pipe(
+    //   switchMap((value: string) => {
+    //     console.log('value');
+    //   })
+    // )
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
-  applyFilter(event: Event) {
+  onSearchChange(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.filteredData = [];
   }
   onYearChange(year: string) {
-
   }
 }
--- website/projects/scimaps/src/app/shared/components/data-table/data-table.module.ts
+++ website/projects/scimaps/src/app/shared/components/data-table/data-table.module.ts
@@ -7,17 +7,21 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
     MatSelectModule,
+    MatIconModule,
   ],
   declarations: [DataTableComponent],
   exports: [DataTableComponent]
