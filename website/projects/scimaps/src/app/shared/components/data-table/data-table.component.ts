7969d7759771e3d8a15c9702992887f5deb8d3ab
swapnil <ddevil691@gmail.com>
2021-06-01 16:44:41 -0400

    pbi-about-page completed table filtering

--- website/projects/scimaps/src/app/shared/components/data-table/data-table.component.ts
+++ website/projects/scimaps/src/app/shared/components/data-table/data-table.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { switchMap } from 'rxjs/operators';
+import { of, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
 import { NewsItem } from '../news-item/news-item.model';
 
 @Component({
@@ -11,45 +12,86 @@ import { NewsItem } from '../news-item/news-item.model';
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent implements OnInit, AfterViewInit {
+export class DataTableComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() data: NewsItem[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   dataSource = new MatTableDataSource(this.data);
-  yearList = ['2021', '2020']
-  selectedYear = '2021';
+  yearList = ['2021', '2020', '2006']
   columns = ['date', 'title', 'publication', 'author', 'pdfLink'];
   
   searchForm!: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) { }
+  searchChangeSubscription: Subscription | undefined;
+  yearChangeSubscription: Subscription | undefined;
+  
+  constructor(private formBuilder: FormBuilder) { 
+    
+  }
 
   ngOnInit(): void {
     this.dataSource.data = this.data;
+    
+    this.dataSource.filterPredicate = this.filterData;
     this.searchForm = this.formBuilder.group({
       year: this.formBuilder.control(''),
       search: this.formBuilder.control('')
     });
-
-    // this.searchForm.get('year')?.valueChanges.pipe(
-    //   switchMap((value: string) => {
-    //     console.log('value');
-    //   })
-    // )
-    // this.dataSource.
+    this.searchForm.setValue({
+      year: '',
+      search: ''
+    })
+    
+    this.searchChangeSubscription = this.searchForm.get('search')?.valueChanges.pipe(
+      filter(searchKey => searchKey),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((searchKey: string) => {
+        return of(searchKey);
+      })
+    ).subscribe((searchKey: string) => {
+      const year = this.searchForm.get('year')?.value;
+      this.dataSource.filter = JSON.stringify({year, searchKey})
+    });
+    
+    this.yearChangeSubscription = this.searchForm.get('year')?.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((year: string) => {
+        return of(year);
+      })
+    ).subscribe((year: string) => {
+      let searchKey = this.searchForm.get('search')?.value;
+      searchKey = searchKey.trim().toLowerCase();
+      this.dataSource.filter = JSON.stringify({year, searchKey})
+    });
   }
+
+  filterData(item: NewsItem, filter: string) {
+    const parsedFilter = JSON.parse(filter);
+    let result = true;
+    if (parsedFilter.year) {
+      const year = new Date(item.date).getFullYear().toString();
+      result = result && (year === parsedFilter.year);
+    }
+    if (parsedFilter.searchKey) {
+      result = result && (item.title.toLowerCase().includes(parsedFilter.searchKey) || item.publication.toLowerCase().includes(parsedFilter.searchKey))
+    }
+    return result;
+  }
+
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
-  onSearchChange(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    this.dataSource.filteredData = [];
-  }
-  onYearChange(year: string) {
+  ngOnDestroy(): void {
+    if (this.searchChangeSubscription) {
+      this.searchChangeSubscription.unsubscribe();
+    }
+    if (this.yearChangeSubscription) {
+      this.yearChangeSubscription.unsubscribe();
+    }
   }
 }
