c068f32aed459afac0bd964ebb632a630a0514e4
swapnil <ddevil691@gmail.com>
2021-06-08 13:00:18 -0400

    pbi-about removed table component

--- website/projects/scimaps/src/app/pages/about/about.module.ts
+++ website/projects/scimaps/src/app/pages/about/about.module.ts
@@ -6,7 +6,6 @@ import { AboutComponent } from './about.component';
 import { ProfileGalleryModule } from '../../shared/components/profile-gallery/profile-gallery.module';
 import { TabHeaderModule } from '../../shared/components/tab-header/tab-header.module';
 import { NewsItemListModule } from '../../shared/components/news-item-list/news-item-list.module';
-import { NewsTableModule } from '../../shared/components/news-table/news-table.module';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { SharedModule } from '../../shared/shared.module';
     ProfileGalleryModule,
     TabHeaderModule,
     NewsItemListModule,
-    NewsTableModule,
     SharedModule
   ]
 })
