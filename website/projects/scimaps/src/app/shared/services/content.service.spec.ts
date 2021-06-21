187be570931a14242e2584348f49bd03135a0117
swapnil <ddevil691@gmail.com>
2021-06-21 15:42:01 -0400

    added spec for content service

--- website/projects/scimaps/src/app/shared/services/content.service.spec.ts
+++ website/projects/scimaps/src/app/shared/services/content.service.spec.ts
@@ -1,27 +1,78 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpRequest } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { EMPTY } from 'rxjs';
 
 import { ContentService } from './content.service';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('ContentService', () => {
   let service: ContentService;
-  let httpGet: jasmine.Spy;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    httpGet = jasmine.createSpy('get', () => EMPTY);
-
     TestBed.configureTestingModule({
-      providers: [{
-        provide: HttpClient,
-        useValue: { get: httpGet }
-      }]
+      imports: [HttpClientTestingModule],
+      providers: [ContentService],
     });
     service = TestBed.inject(ContentService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should make call to index.json when index = index (without extension)', () => {
+    const indexName = 'people';
+    service.getIndex(indexName).subscribe((data) => {
+      expect(data).toBeTruthy();
+    });
+    const req = httpTestingController.expectOne(
+      `assets/indexes/${indexName}.json`
+    );
+    expect(req.request.method).toEqual('GET');
+    req.flush({});
+  });
+
+  it('should make call to index.json when index = index.json', () => {
+    const indexName = 'people';
+    service.getIndex(indexName).subscribe((data) => {
+      expect(data).toBeTruthy();
+    });
+    const req = httpTestingController.expectOne(`assets/indexes/${indexName}.json`);
+    expect(req.request.method).toEqual('GET');
+    req.flush({
+      name: 'Achim Oberg',
+      last_name: 'Oberg',
+      image: 'image.jpg',
+      roles: ['maker'],
+    });
+  });
+
+  it('should call to readme.md file when path does NOT end with readme.md', () => {
+    const path = 'person/achim-oberg';
+    service.getContent(path).subscribe((data) => {
+      expect(data).toBeTruthy();
+    });
+    const req = httpTestingController.expectOne(`assets/content/${path}/readme.md`);
+    expect(req.request.method).toEqual('GET');
+    req.flush({});
+  });
+
+  it('should call to readme.md file when path ends with readme', () => {
+    const path = 'person/achim-oberg/readme';
+    service.getContent(path).subscribe((data) => {
+      expect(data).toBeTruthy();
+    });
+    const req = httpTestingController.expectOne(`assets/content/${path}.md`);
+    expect(req.request.method).toEqual('GET');
+    req.flush({});
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
