362e2e1f5de1e75bbc5ff74863871bdbe2717490
swapnil <ddevil691@gmail.com>
2021-06-22 11:48:42 -0400

    profile-item component with showless/show more

--- website/projects/scimaps/src/app/shared/components/profile-item/profile-item.component.ts
+++ website/projects/scimaps/src/app/shared/components/profile-item/profile-item.component.ts
@@ -4,16 +4,43 @@ import { Profile } from '../../../core/models/profile';
 @Component({
   selector: 'sci-profile-item',
   templateUrl: './profile-item.component.html',
-  styleUrls: ['./profile-item.component.scss']
+  styleUrls: ['./profile-item.component.scss'],
 })
 export class ProfileItemComponent implements OnInit {
-
   @Input() profile!: Profile;
   @Input() compact = true;
 
-  constructor() { }
+  maxContentLength = 100;
 
-  ngOnInit(): void {
+  /** Whether the book content exceeds the maximum character count */
+  get hasLongContent(): boolean {
+    return (this.profile?.body?.length || 0) > this.maxContentLength;
   }
 
+  /** Full profile content */
+  get fullContent(): string {
+    return this.profile.body;
+  }
+
+  /** Profile content - truncated if necessary */
+  get partialContent(): string {
+    const {
+      fullContent,
+      maxContentLength,
+      hasLongContent,
+      fullContentVisible,
+    } = this;
+
+    if (!hasLongContent || fullContentVisible) {
+      return fullContent;
+    }
+    return `${fullContent.slice(0, maxContentLength)}...`;
+  }
+
+  fullContentVisible = false;
+  
+  constructor() {}
+  
+  ngOnInit(): void {
+  }
 }
