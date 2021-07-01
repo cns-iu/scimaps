b5b83113a6b7b7ef0e1b9aa906e948140df03282
swapnil <ddevil691@gmail.com>
2021-07-01 13:14:52 -0400

    fixed specs

--- website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes-body-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes-body-resolver.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ContentService } from '../../shared/services/content.service';
+import { of } from 'rxjs';
+import { Params } from '@angular/router';
 import { CallForMacroscopesBodyResolver } from './call-for-macroscopes-body-resolver.service';
 
 describe('CallForMacroscopesBody', () => {
   let service: CallForMacroscopesBodyResolver;
-
+  let contentService: Params;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', ['getContent']);
+    TestBed.configureTestingModule({
+      providers: [
+        CallForMacroscopesBodyResolver,
+        {provide: ContentService, useValue: contentServiceSpy}
+      ]
+    });
     service = TestBed.inject(CallForMacroscopesBodyResolver);
+    contentService = TestBed.inject(ContentService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call contentService.getContent once', () => {
+    (contentService.getContent as jasmine.Spy).and.returnValue(
+      of({
+        lastiteration: 16,
+        tabs: []
+      })
+    );
+    service.resolve();
+    expect(contentService.getContent).toHaveBeenCalledTimes(1);
+    expect(contentService.getContent).toHaveBeenCalledWith('site/callForMacroscopes.md');
   });
 });
