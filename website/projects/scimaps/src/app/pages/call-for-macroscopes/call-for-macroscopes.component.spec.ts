c4e200b10a8655d5cfbc4c1b5997636931d8f12d
swapnil <ddevil691@gmail.com>
2021-07-01 14:45:52 -0400

    lint issues

--- website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes.component.spec.ts
+++ website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes.component.spec.ts
@@ -1,29 +1,28 @@
-import { DebugElement } from "@angular/core";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { CallForMacroscopesComponent } from "./call-for-macroscopes.component";
-import { CallForMacroscopesModule } from "./call-for-macroscopes.module";
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CallForMacroscopesComponent } from './call-for-macroscopes.component';
+import { CallForMacroscopesModule } from './call-for-macroscopes.module';
 
 describe('CallForMacroscopesComponent', () => {
-    let component: CallForMacroscopesComponent;
-    let fixture: ComponentFixture<CallForMacroscopesComponent>;
-    let el: DebugElement;
-  
-    beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        imports: [CallForMacroscopesModule, RouterTestingModule]
-      })
-      .compileComponents();
-    });
-  
-    beforeEach(() => {
-      fixture = TestBed.createComponent(CallForMacroscopesComponent);
-      component = fixture.componentInstance;
-      el = fixture.debugElement;
-    });
-  
-    it('should create component', () => {
-      fixture.detectChanges();
-      expect(component).toBeTruthy();
-    });
+  let component: CallForMacroscopesComponent;
+  let fixture: ComponentFixture<CallForMacroscopesComponent>;
+  let el: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CallForMacroscopesModule, RouterTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CallForMacroscopesComponent);
+    component = fixture.componentInstance;
+    el = fixture.debugElement;
+  });
+
+  it('should create component', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
 });
