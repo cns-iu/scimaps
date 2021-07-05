85e7882111c42bb93e930ad013e37e7c01d1b89b
swapnil <ddevil691@gmail.com>
2021-07-05 12:04:29 -0400

    moved path updation in separate function

--- website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.ts
@@ -21,22 +21,29 @@ export class HostingBodyResolverService implements Resolve<HostingBody> {
   directory = 'assets/content/site/hosting';
   constructor(private content: ContentService) { }
 
+
   resolve(): HostingBody | Observable<HostingBody> {
     return this.content.getContent<HostingBody>(this.mdPath).pipe(
       take(1),
       map((body: HostingBody) => {
-        if (body.install_guide) {
-          if (!isHttp(body.install_guide)) {
-            body.install_guide = `${this.directory}/${body.install_guide}`;
-          }
-        }
-        if (body.master_booklet) {
-          if (!isHttp(body.master_booklet)) {
-            body.master_booklet = `${this.directory}/${body.master_booklet}`;
-          }
-        }
-        return body;
+        return this.updatePaths(body);
       })
     );
+  }
+
+ 
+  // converts paths to absolute paths.
+  updatePaths(body: HostingBody) {
+    if (body.install_guide) {
+      if (!isHttp(body.install_guide)) {
+        body.install_guide = `${this.directory}/${body.install_guide}`;
+      }
+    }
+    if (body.master_booklet) {
+      if (!isHttp(body.master_booklet)) {
+        body.master_booklet = `${this.directory}/${body.master_booklet}`;
+      }
+    }
+    return body;
   }
 }
