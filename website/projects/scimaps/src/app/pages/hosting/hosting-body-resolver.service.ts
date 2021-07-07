965cc76fe5cb2bfa88e2bc1438c459b1d738771a
swapnil <ddevil691@gmail.com>
2021-07-07 17:28:30 -0400

    merged into one iff

--- website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.ts
@@ -6,6 +6,7 @@ import { isHttp } from '../../constants/utils';
 import { ContentService } from '../../shared/services/content.service';
 
 export interface HostingBody {
+  tabs: Array<{image: string}>
   overview: string;
   carousel: {sm: string, lg: string}[];
   install_guide: string;
@@ -34,32 +35,32 @@ export class HostingBodyResolverService implements Resolve<HostingBody> {
  
   // converts paths to absolute paths.
   updatePaths(body: HostingBody): HostingBody {
-    if (body.install_guide) {
-      if (!isHttp(body.install_guide)) {
-        body.install_guide = `${this.directory}/${body.install_guide}`;
-      }
+    if (body.install_guide && !isHttp(body.install_guide)) {
+      body.install_guide = `${this.directory}/${body.install_guide}`;
     }
-    if (body.master_booklet) {
-      if (!isHttp(body.master_booklet)) {
-        body.master_booklet = `${this.directory}/${body.master_booklet}`;
-      }
+    if (body.master_booklet && !isHttp(body.master_booklet)) {
+      body.master_booklet = `${this.directory}/${body.master_booklet}`;
     }
+    // Carousel
     const {carousel} = body;
     if (carousel && Array.isArray(carousel)) {
       carousel.forEach(image => {
-        if (image.lg) {
-          if (!isHttp(image.lg)) {
-            image.lg = `${this.directory}/${image.lg}`;  
-          }
+        if (image.lg && !isHttp(image.lg)) {
+          image.lg = `${this.directory}/${image.lg}`;  
         }
-        if (image.sm) {
-          if (!isHttp(image.sm)) {
-            image.sm = `${this.directory}/${image.sm}`;  
-          }
+        if (image.sm && !isHttp(image.sm)) {
+          image.sm = `${this.directory}/${image.sm}`;
         }
       });
       body.carousel = carousel;
     }
+    // Tabs
+    const {tabs} = body;
+    tabs.forEach((tab) => {
+      if (tab.image && !isHttp(tab.image)) {
+        tab.image = `${this.directory}/${tab.image}`
+      }
+    })
     return body;
   }
 }
