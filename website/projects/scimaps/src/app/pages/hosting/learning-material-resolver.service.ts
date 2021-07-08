3334d066b0fa9f8fe42cb2eb3d99289275245f0e
swapnil <ddevil691@gmail.com>
2021-07-07 20:37:38 -0400

    added resolver for learning material

--- website/projects/scimaps/src/app/pages/hosting/learning-material-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/hosting/learning-material-resolver.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
+interface LearningMaterial {
+  title: string;
+  content: string;
+  image: {
+    sm: string,
+    lg: string
+  };
+}
 @Injectable({
   providedIn: 'root'
 })
-export class LearningMaterialResolverService {
+export class LearningMaterialResolverService implements Resolve<LearningMaterial[]> {
 
   constructor() { }
+
+
+  resolve(): LearningMaterial[] | Observable<LearningMaterial[]> | Promise<LearningMaterial[]> {
+    throw new Error('Method not implemented.');
+  }
 }
