4b04849fa5836c81d13f33d6b60738881a66ef3d
swapnil <ddevil691@gmail.com>
2021-07-12 11:53:03 -0400

    added modules

--- website/projects/scimaps/src/app/pages/venues/venues.module.ts
+++ website/projects/scimaps/src/app/pages/venues/venues.module.ts
@@ -6,15 +6,31 @@ import { VenuesComponent } from './venues.component';
 
 import { LazyCardsModule } from '../../shared/components/lazy-cards/lazy-cards.module';
 import { LazyTableModule } from '../../shared/components/lazy-table/lazy-table.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [VenuesComponent],
   imports: [
     CommonModule,
     VenuesRoutingModule,
-    LazyCardsModule,
-    LazyTableModule
+    // LazyCardsModule,
+    // LazyTableModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    LazyTableModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatIconModule,
   ]
 })
 export class VenuesModule { }
