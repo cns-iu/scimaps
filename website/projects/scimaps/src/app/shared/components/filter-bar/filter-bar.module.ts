384b878dd4152799c7bf192e84a84f40b7b6d3bb
swapnil <ddevil691@gmail.com>
2021-07-13 13:17:42 -0400

    added filterbar component

---
+++ website/projects/scimaps/src/app/shared/components/filter-bar/filter-bar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilterbarComponent } from './filterbar.component';
+
+
+
+@NgModule({
+  declarations: [FilterbarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FilterBarModule { }
---
+++ website/projects/scimaps/src/app/shared/components/filter-bar/filterbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sci-filterbar',
+  templateUrl: './filterbar.component.html',
+  styleUrls: ['./filterbar.component.scss']
+})
+export class FilterbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
