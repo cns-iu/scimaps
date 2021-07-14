17000c7cd695bc7a04508ad93609d2b72dcf1756
swapnil <ddevil691@gmail.com>
2021-07-14 10:41:43 -0400

    scrollup on init

--- website/projects/scimaps/src/app/shared/components/drawer/drawer.component.ts
+++ website/projects/scimaps/src/app/shared/components/drawer/drawer.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sci-drawer',
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.scss'],
 })
-export class DrawerComponent {
+export class DrawerComponent implements OnInit {
+  ngOnInit() {
+    window.scroll({top: 0, left: 0, behavior: 'smooth'});
+  }
 }
