7c2eb1e666512ba426dbd0f733ab4a93e8b695ff
swapnil <ddevil691@gmail.com>
2021-07-14 18:52:35 -0400

    removed news-item list component

--- website/projects/scimaps/src/app/pages/about/about.module.ts
+++ website/projects/scimaps/src/app/pages/about/about.module.ts
@@ -5,7 +5,6 @@ import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { ProfileGalleryModule } from '../../shared/components/profile-gallery/profile-gallery.module';
 import { TabHeaderModule } from '../../shared/components/tab-header/tab-header.module';
-import { NewsItemListModule } from '../../shared/components/news-item-list/news-item-list.module';
 import { SharedModule } from '../../shared/shared.module';
 import { MatSelectModule } from '@angular/material/select';
 import { MarkdownModule } from 'ngx-markdown';
@@ -22,7 +21,6 @@ import { ListViewModule } from '../../shared/components/list-view/list-view.modu
     AboutRoutingModule,
     ProfileGalleryModule,
     TabHeaderModule,
-    NewsItemListModule,
     MatSelectModule,
     SharedModule,
     MarkdownModule,
