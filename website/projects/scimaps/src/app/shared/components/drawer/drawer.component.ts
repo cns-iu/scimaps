826a550f0608998eb22759ffd99bc40d620cb7ff
swapnil <ddevil691@gmail.com>
2021-07-15 12:38:41 -0400

    DRAWER FIX

--- website/projects/scimaps/src/app/shared/components/drawer/drawer.component.ts
+++ website/projects/scimaps/src/app/shared/components/drawer/drawer.component.ts
@@ -5,8 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.scss'],
 })
-export class DrawerComponent implements OnInit {
-  ngOnInit() {
-    window.scroll({top: 0, left: 0, behavior: 'smooth'});
-  }
+export class DrawerComponent {
 }
