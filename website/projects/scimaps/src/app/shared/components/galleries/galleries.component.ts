b8bd4bf9e4ac37a696f363476a92e3e6d7a2ac40
swapnil <ddevil691@gmail.com>
2021-07-18 21:12:10 -0400

    Added galleries component

---
+++ website/projects/scimaps/src/app/shared/components/galleries/galleries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sci-galleries',
+  templateUrl: './galleries.component.html',
+  styleUrls: ['./galleries.component.scss']
+})
+export class GalleriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ website/projects/scimaps/src/app/shared/components/galleries/galleries.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GalleriesComponent } from '../galleries.component';
+
+
+
+@NgModule({
+  declarations: [GalleriesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GalleriesModule { }
