d06643aabe9a65512166a8391fe6b0bc2b15f667
swapnil <ddevil691@gmail.com>
2021-07-21 16:00:56 -0400

    App state setup

---
+++ website/projects/scimaps/src/app/core/models/app.model.ts
@@ -0,0 +1,4 @@
+export interface AppState {
+    test: string;
+    showDrawer: boolean;
+};
--- website/projects/scimaps/src/app/core/state/page/page.state.ts
+++ website/projects/scimaps/src/app/core/state/page/page.state.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Params } from '@angular/router';
 import { StateRepository } from '@ngxs-labs/data/decorators';
 import { NgxsImmutableDataRepository } from '@ngxs-labs/data/repositories';
-import { State } from '@ngxs/store';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { SetAppState } from '../../actions/app.actions';
+import { AppState } from '../../models/app.model';
 
 
 interface PageStateModel {
   placeholder: unknown; // Remove when other fields are added
+  app: AppState
 }
 
 @StateRepository()
 @State<PageStateModel>({
   name: 'page',
   defaults: {
-    placeholder: 0
+    placeholder: 0,
+    app: {
+      test: 'test',
+      showDrawer: false
+    }
   }
 })
 @Injectable()
 export class PageState extends NgxsImmutableDataRepository<PageStateModel> {
+  @Selector()
+  static getAppState(state: PageStateModel) {
+    return state.app;
+  }
+
+  @Action(SetAppState)
+  setAppState({getState, patchState}: StateContext<PageStateModel>, {payload}: SetAppState) {
+    const state = getState();
+    patchState({
+      app: {...state.app, ...payload}
+    })
+  }
 }
