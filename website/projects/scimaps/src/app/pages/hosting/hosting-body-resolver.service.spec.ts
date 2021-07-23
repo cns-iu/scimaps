f25ce453b8180c4e4570bc7687c1a8a67f0dd53f
swapnil <ddevil691@gmail.com>
2021-07-23 12:25:53 -0400

    spec fixes

--- website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { HostingBodyResolverService } from './hosting-body-resolver.service';
 
-describe('HostingBodyResolver.Service.TsService', () => {
+describe('HostingBodyResolverService', () => {
   let service: HostingBodyResolverService;
 
   beforeEach(() => {
--- website/projects/scimaps/src/app/shared/components/gallery-item/gallery-item.component.spec.ts
+++ website/projects/scimaps/src/app/shared/components/gallery-item/gallery-item.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Venue } from '../../../pages/venues/venues-resolver.service';
 
 import { GalleryItemComponent } from './gallery-item.component';
+import { GalleryItemModule } from './gallery-item.module';
 
+export const getVenues = (n: number) => {
+  const result: Venue[] = []
+  for (let i=0; i< n; i ++) {
+    result.push({
+      dateStart: `2012-02-0${i}`,
+      dateEnd: '',
+      title: `Title ${i}`,
+      venue: `Venue ${i}`,
+      city: `City ${i}`,
+      pdfLink: `pdfLink ${i}`,
+      organizer: `organizer ${i}`,
+      credit: `credit ${i}`,
+      state: `State ${i}`,
+      country: `country ${i}`,
+      venueImages: [{
+        sm: `image${i}.sm.jpg`,
+        lg: `image${i}.lg.jpg`
+      }]
+    });
+  }
+  return result;
+}
 describe('GalleryItemComponent', () => {
   let component: GalleryItemComponent;
   let fixture: ComponentFixture<GalleryItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GalleryItemComponent ]
+      declarations: [ GalleryItemComponent ],
+      imports: [GalleryItemModule]
     })
     .compileComponents();
   });
@@ -16,6 +41,8 @@ describe('GalleryItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GalleryItemComponent);
     component = fixture.componentInstance;
+    const venue = getVenues(1)[0];
+    component.item = venue;
     fixture.detectChanges();
   });
 
