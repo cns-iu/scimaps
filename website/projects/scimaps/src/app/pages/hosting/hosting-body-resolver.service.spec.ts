46d21680f8d9c9aa6b7e36a15483aa49c8f2007c
swapnil <ddevil691@gmail.com>
2021-07-23 16:22:09 -0400

    hosting body resolver service spec boilerplate

--- website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/hosting/hosting-body-resolver.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { Params } from '@angular/router';
+import { ContentService } from '../../shared/services/content.service';
 
 import { HostingBodyResolverService } from './hosting-body-resolver.service';
 
 describe('HostingBodyResolverService', () => {
   let service: HostingBodyResolverService;
-
+  let contentService: Params;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', ['getContent']);
+    TestBed.configureTestingModule({
+      providers: [
+        HostingBodyResolverService,
+        {provide: ContentService, useValue: contentServiceSpy}
+      ]
+    });
     service = TestBed.inject(HostingBodyResolverService);
+    contentService = TestBed.inject(ContentService);
   });
 
   it('should be created', () => {
