af5be54b3c1acadc6e76fd970ea6e5268d59226b
swapnil <ddevil691@gmail.com>
2021-07-23 15:47:51 -0400

    fixed spec

--- website/projects/scimaps/src/app/shared/components/table/table.component.spec.ts
+++ website/projects/scimaps/src/app/shared/components/table/table.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsModule, Store } from '@ngxs/store';
+import { TabHeaderModule } from '../tab-header/tab-header.module';
 
 import { TableComponent } from './table.component';
+import { TableModule } from './table.module';
 
 describe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
+      declarations: [ TableComponent ],
+      imports: [TableModule, BrowserAnimationsModule, NgxsModule.forRoot([])]
     })
     .compileComponents();
   });
