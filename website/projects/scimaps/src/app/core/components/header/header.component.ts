b968356065470b381668d21fd6742c432827edbc
swapnil <ddevil691@gmail.com>
2021-08-05 19:29:20 -0400

    nav menu issues fixed

--- website/projects/scimaps/src/app/core/components/header/header.component.ts
+++ website/projects/scimaps/src/app/core/components/header/header.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { Params } from '@angular/router';
 import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { PageState } from '../../state/page/page.state';
 
 
@@ -14,8 +16,7 @@ import { PageState } from '../../state/page/page.state';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit, AfterViewInit {
   /** HTML classes */
   @HostBinding('class') readonly clsName = 'sci-header mat-display-3';
 
@@ -28,6 +29,47 @@ export class HeaderComponent {
    * Emits whenever the sidenav buttton is clicked
    */
   @Output() sidenavOpenChange = new EventEmitter<boolean>();
+  @ViewChild('exhibitTrigger') exhibitTrigger!: MatMenuTrigger;
+  @ViewChild('contactTrigger') contactTrigger!: MatMenuTrigger;
+  stream: Subject<Params> = new Subject<Params>();
 
   @Select(PageState.drawer) drawer$!: Observable<Params>;
+
+  ngOnInit() {
+    this.stream.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      switchMap((payload: Params) => {
+        return of(payload);
+      })
+    ).subscribe((payload) => {
+      if (payload.index === 0) {
+        if (payload.type === 'mouseenter') {
+          if (this.contactTrigger.menuOpen) {
+            this.contactTrigger.closeMenu();
+          }
+          this.exhibitTrigger.openMenu();
+        } else {
+          this.exhibitTrigger?.closeMenu();
+        }
+      } else if (payload.index === 1) {
+        if (payload.type === 'mouseenter') {
+          if (this.exhibitTrigger.menuOpen) {
+            this.exhibitTrigger.closeMenu();
+          }
+          this.contactTrigger.openMenu();
+        } else {
+          this.contactTrigger.closeMenu();
+        }
+      }
+    });
+  }
+
+  mouseTrigger(e: Event, index: number) {
+    this.stream.next({type: e.type, event: e, index});
+  }
+  ngAfterViewInit(): void {
+    console.log(this.exhibitTrigger);
+    console.log(this.contactTrigger);
+  }
 }
