49bbfe3fe7e2bee5d6c1c7955ac631e741510b62
swapnil <ddevil691@gmail.com>
2021-08-13 01:35:28 -0400

    added more/less toggle t0 maps

--- website/projects/scimaps/src/app/shared/components/discover-listing/discover-listing.component.ts
+++ website/projects/scimaps/src/app/shared/components/discover-listing/discover-listing.component.ts
@@ -3,11 +3,13 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DiscoverItem } from '../../../core/models/discover-item';
 import { WarningDialogComponent } from '../warning-dialog/warning-dialog.component';
+import { contentTrigger } from '../../../constants/content-trigger';
 
 @Component({
   selector: 'sci-discover-listing',
   templateUrl: './discover-listing.component.html',
-  styleUrls: ['./discover-listing.component.scss']
+  styleUrls: ['./discover-listing.component.scss'],
+  animations: [contentTrigger]
 })
 export class DiscoverListingComponent {
   /** HTML class name */
@@ -19,7 +21,26 @@ export class DiscoverListingComponent {
   /** Type of discover list */
   @Input() type!: 'macroscopes' | 'maps';
 
+  fullContentVisible = false;
+  maxContentLength = 500;
+
   constructor(private readonly dialog: MatDialog, private router: Router) { }
+
+  get hasLongContent(): boolean {
+    return (this.discoverItem?.body?.length || 0) > this.maxContentLength;
+  }
+
+  get fullContent(): string {
+    return this.discoverItem.body;
+  }
+
+  get partialContent(): string {
+    const {
+      fullContent,
+      maxContentLength
+    } = this;
+    return `${fullContent.slice(0, maxContentLength)}...`;
+  }
 
   imageSource(image: string): string {
     return image.startsWith('assets/') ? image : `assets/${this.discoverItem.directory}/${this.discoverItem.slug}/${image}`;
