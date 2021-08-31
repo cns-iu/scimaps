8867b37afd5e1b28c714b04da0627fee25311e3d
swapnil <ddevil691@gmail.com>
2021-08-31 15:49:39 -0400

    generated tile component

---
+++ website/projects/scimaps/src/app/shared/components/tile/tile.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TileComponent } from './tile.component';
+
+
+
+@NgModule({
+  declarations: [TileComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TileModule { }
