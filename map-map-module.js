c9baae34e5d92f5c5b900047f88a342a7adc9584
bherr2 <bherr2@users.noreply.github.com>
2021-09-07 17:20:54 +0000

    Production deploy -- 44a074a17cdf34f3d562b6520f30751eb2d23e9c

--- map-map-module.js
+++ map-map-module.js
@@ -52,13 +52,14 @@ class MapComponent {
     }
 }
 MapComponent.ɵfac = function MapComponent_Factory(t) { return new (t || MapComponent)(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_router__WEBPACK_IMPORTED_MODULE_1__["ActivatedRoute"]), _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_router__WEBPACK_IMPORTED_MODULE_1__["Router"])); };
-MapComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: MapComponent, selectors: [["sci-map"]], decls: 1, vars: 2, consts: [[1, "map", 3, "item", "type", "languageChange"]], template: function MapComponent_Template(rf, ctx) { if (rf & 1) {
+MapComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: MapComponent, selectors: [["sci-map"]], decls: 2, vars: 2, consts: [[1, "map", 3, "item", "type", "languageChange"], ["detail", ""]], template: function MapComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "sci-item-drawer", 0);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("languageChange", function MapComponent_Template_sci_item_drawer_languageChange_0_listener($event) { return ctx.changeLanguage($event); });
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](1, "router-outlet", 1);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
     } if (rf & 2) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵproperty"]("item", ctx.selectedItem)("type", "map");
-    } }, directives: [_shared_components_item_drawer_item_drawer_component__WEBPACK_IMPORTED_MODULE_2__["ItemDrawerComponent"]], styles: ["[_nghost-%COMP%] {\n  position: static;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInByb2plY3RzL3NjaW1hcHMvc3JjL2FwcC9wYWdlcy9tYXAvbWFwLmNvbXBvbmVudC5zY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQ0ksZ0JBQUE7QUFDSiIsImZpbGUiOiJwcm9qZWN0cy9zY2ltYXBzL3NyYy9hcHAvcGFnZXMvbWFwL21hcC5jb21wb25lbnQuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIjpob3N0IHtcbiAgICBwb3NpdGlvbjogc3RhdGljO1xufSJdfQ== */"] });
+    } }, directives: [_shared_components_item_drawer_item_drawer_component__WEBPACK_IMPORTED_MODULE_2__["ItemDrawerComponent"], _angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterOutlet"]], styles: ["[_nghost-%COMP%] {\n  position: static;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInByb2plY3RzL3NjaW1hcHMvc3JjL2FwcC9wYWdlcy9tYXAvbWFwLmNvbXBvbmVudC5zY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQ0ksZ0JBQUE7QUFDSiIsImZpbGUiOiJwcm9qZWN0cy9zY2ltYXBzL3NyYy9hcHAvcGFnZXMvbWFwL21hcC5jb21wb25lbnQuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIjpob3N0IHtcbiAgICBwb3NpdGlvbjogc3RhdGljO1xufSJdfQ== */"] });
 /*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](MapComponent, [{
         type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"],
         args: [{
@@ -86,6 +87,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _map_routing_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./map-routing.module */ "oLE7");
 /* harmony import */ var _map_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./map.component */ "5T7I");
 /* harmony import */ var _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../shared/components/item-drawer/item-drawer.module */ "ml3Y");
+/* harmony import */ var _shared_components_tile_tile_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../shared/components/tile/tile.module */ "KJZk");
+
 
 
 
@@ -98,11 +101,13 @@ MapModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule
 MapModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ factory: function MapModule_Factory(t) { return new (t || MapModule)(); }, imports: [[
             _angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"],
             _map_routing_module__WEBPACK_IMPORTED_MODULE_2__["MapRoutingModule"],
-            _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"]
+            _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"],
+            _shared_components_tile_tile_module__WEBPACK_IMPORTED_MODULE_5__["TileModule"]
         ]] });
 (function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](MapModule, { declarations: [_map_component__WEBPACK_IMPORTED_MODULE_3__["MapComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"],
         _map_routing_module__WEBPACK_IMPORTED_MODULE_2__["MapRoutingModule"],
-        _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"]] }); })();
+        _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"],
+        _shared_components_tile_tile_module__WEBPACK_IMPORTED_MODULE_5__["TileModule"]] }); })();
 /*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](MapModule, [{
         type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
         args: [{
@@ -110,7 +115,8 @@ MapModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector
                 imports: [
                     _angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"],
                     _map_routing_module__WEBPACK_IMPORTED_MODULE_2__["MapRoutingModule"],
-                    _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"]
+                    _shared_components_item_drawer_item_drawer_module__WEBPACK_IMPORTED_MODULE_4__["ItemDrawerModule"],
+                    _shared_components_tile_tile_module__WEBPACK_IMPORTED_MODULE_5__["TileModule"]
                 ]
             }]
     }], null, null); })();
@@ -130,22 +136,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MapRoutingModule", function() { return MapRoutingModule; });
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "EM62");
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "sEIs");
-/* harmony import */ var _shared_services_map_macroscope_resolver_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../shared/services/map-macroscope-resolver.service */ "UTmY");
-/* harmony import */ var _map_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./map.component */ "5T7I");
+/* harmony import */ var _shared_components_tile_tile_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../shared/components/tile/tile.component */ "B5uJ");
+/* harmony import */ var _shared_services_map_macroscope_resolver_service__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../shared/services/map-macroscope-resolver.service */ "UTmY");
+/* harmony import */ var _map_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./map.component */ "5T7I");
 
 
 
 
 
 
-const routes = [{
+
+const routes = [
+    {
         path: ':iteration/:sequence',
-        component: _map_component__WEBPACK_IMPORTED_MODULE_3__["MapComponent"],
+        component: _map_component__WEBPACK_IMPORTED_MODULE_4__["MapComponent"],
         resolve: {
-            map: _shared_services_map_macroscope_resolver_service__WEBPACK_IMPORTED_MODULE_2__["MapMacroscopeResolverService"]
+            map: _shared_services_map_macroscope_resolver_service__WEBPACK_IMPORTED_MODULE_3__["MapMacroscopeResolverService"],
         },
-        runGuardsAndResolvers: 'paramsOrQueryParamsChange'
-    }];
+        runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+        children: [
+            {
+                path: 'detail',
+                component: _shared_components_tile_tile_component__WEBPACK_IMPORTED_MODULE_2__["TileComponent"],
+            },
+        ],
+    },
+];
 class MapRoutingModule {
 }
 MapRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: MapRoutingModule });
@@ -155,7 +171,7 @@ MapRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineI
         type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
         args: [{
                 imports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"].forChild(routes)],
-                exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]]
+                exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterModule"]],
             }]
     }], null, null); })();
 
