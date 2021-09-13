6cdfaee673def68ae5f8174ed3f703bca30c0c31
SwapnilP <ddevil691@gmail.com>
2021-09-13 09:36:30 -0400

    Pbi sept (#114)
    * style changes in form
    
    * tile height issue fixed
    
    * changed title
    
    * minor UI changes
    
    * making different route for maker video
    
    * completed video route
    
    * Added styles for maker-video
    
    * fixes in content url
    
    * spacing issue fixed
    
    * hiding detail drawer on tablet and desktop
    
    * fixed linting issues
    
    * spec fixes
    
    * lint issues
    
    * removed unwanted import
    
    * removed unwanted imports
    
    * updated content

--- website/projects/scimaps/src/app/pages/maker-videos/maker-videos-routing.module.ts
+++ website/projects/scimaps/src/app/pages/maker-videos/maker-videos-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MakerVideoComponent } from '../../shared/components/maker-video/maker-video.component';
 import { MakerVideosBodyResolverService } from './maker-videos-body-resolver.service';
 import { MakerVideosResolverService } from './maker-videos-resolver.service';
 import { MakerVideosComponent } from './maker-videos.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MakerVideosComponent,
-  resolve: {
-    body: MakerVideosBodyResolverService,
-    videos: MakerVideosResolverService
+const routes: Routes = [
+  {
+    path: '',
+    component: MakerVideosComponent,
+    resolve: {
+      body: MakerVideosBodyResolverService,
+      videos: MakerVideosResolverService
+    },
+    children: [
+      {
+        path: ':slug',
+        component: MakerVideoComponent
+      }
+    ]
   }
-}];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
