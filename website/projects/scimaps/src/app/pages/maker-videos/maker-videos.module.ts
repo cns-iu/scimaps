3ebf54805fea39ff963d31418e47393ddc031560
swapnil <ddevil691@gmail.com>
2021-09-13 08:46:31 -0400

    removed unwanted import

--- website/projects/scimaps/src/app/pages/maker-videos/maker-videos.module.ts
+++ website/projects/scimaps/src/app/pages/maker-videos/maker-videos.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { MakerVideosRoutingModule } from './maker-videos-routing.module';
-import { MakerVideosComponent } from './maker-videos.component';
+import { NgModule } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 import { DrawerHeaderModule } from '../../shared/components/drawer-header/drawer-header.module';
 import { DrawerModule } from '../../shared/components/drawer/drawer.module';
-import { SafePipe } from '../../pipes/safe.pipe';
 import { MakerVideoModule } from '../../shared/components/maker-video/maker-video.module';
+import { MakerVideosRoutingModule } from './maker-videos-routing.module';
+import { MakerVideosComponent } from './maker-videos.component';
+
 
 
 @NgModule({
