9f56d975548743a37860056df27aa63726c985a3
swapnil <ddevil691@gmail.com>
2021-09-17 16:16:01 -0400

    updated link

--- website/projects/scimaps/src/app/pages/home/home.component.ts
+++ website/projects/scimaps/src/app/pages/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent {
     subtitle: '',
     body: 'The 17th Iteration of the Places & Spaces exhibit launches on Sept 24 at the University of Notre Dame. Register here to join a virtual talk by Curator Katy Borner and virtual tour of the new macroscopes with co-Curator Todd Theriault.',
     buttonLabel: 'Register',
-    buttonLink: 'https://library.nd.edu/events',
+    buttonLink: 'https://library.nd.edu/event/maps-and-macroscopes-2021-09-24',
     imageSource: 'assets/action-card/actioncard.jpg'
   };
 }
