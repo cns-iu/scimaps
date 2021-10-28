67aabeaa7165841674df3095ce74544592b71499
swapnil <ddevil691@gmail.com>
2021-10-28 02:00:03 -0400

    Added drawer content

--- website/projects/scimaps/src/app/shared/components/blog/blog.component.ts
+++ website/projects/scimaps/src/app/shared/components/blog/blog.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { drawerInOut } from '../../../constants/drawer.animations';
 
 @Component({
   selector: 'sci-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  animations: [drawerInOut],
 })
 export class BlogComponent implements OnInit {
-
-  constructor() { }
+  showDrawer = false;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.showDrawer = true;
   }
 
+  goBack() {
+    this.showDrawer = false;
+    setTimeout(() => {
+      this.router.navigate(['/', 'learning-center', 'blogs']);
+    }, 500);
+  }
 }
--- website/projects/scimaps/src/app/shared/components/blog/blog.module.ts
+++ website/projects/scimaps/src/app/shared/components/blog/blog.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { DrawerModule } from '../drawer/drawer.module';
+import { DrawerHeaderModule } from '../drawer-header/drawer-header.module';
 
 
 @NgModule({
   declarations: [BlogComponent],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    DrawerModule,
+    DrawerHeaderModule
   ],
   exports: [BlogComponent]
 })
