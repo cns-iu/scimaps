65055068cd68468a8df5dc367fa5b524f49538a2
swapnil <ddevil691@gmail.com>
2021-11-01 12:25:29 -0400

    Added videos in routing

--- website/projects/scimaps/src/app/pages/learning-center/learning-center-routing.module.ts
+++ website/projects/scimaps/src/app/pages/learning-center/learning-center-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MakerVideosResolverService } from '../maker-videos/maker-videos-resolver.service';
 import { BlogsResolverService } from './blogs-resolver.service';
 import { LearningCenterBodyResolverService } from './learning-center-body-resolver.service';
 import { LearningCenterComponent } from './learning-center.component';
@@ -10,12 +11,16 @@ const routes: Routes = [
     component: LearningCenterComponent,
     resolve: {
       body: LearningCenterBodyResolverService,
-      blogs: BlogsResolverService
+      blogs: BlogsResolverService,
+      videos: MakerVideosResolverService
     },
     children: [
       {
         path: 'blogs',
         loadChildren: () => import('../blogs/blogs.module').then(m => m.BlogsModule)
+      }, {
+        path: 'videos',
+        loadChildren: () => import('../maker-videos/maker-videos.module').then(m => m.MakerVideosModule)
       }
     ]
   }
