a93a14f3153d800f658996f09214b0bdffa79ca3
swapnil <ddevil691@gmail.com>
2021-11-01 14:11:31 -0400

    added carousel

--- website/projects/scimaps/src/app/shared/components/blog/blog.module.ts
+++ website/projects/scimaps/src/app/shared/components/blog/blog.module.ts
@@ -7,6 +7,7 @@ import { DrawerModule } from '../drawer/drawer.module';
 import { DrawerHeaderModule } from '../drawer-header/drawer-header.module';
 import { MarkdownModule } from 'ngx-markdown';
 import { SharedModule } from '../../shared.module';
+import { CarouselModule } from '../carousel/carousel.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { SharedModule } from '../../shared.module';
     DrawerModule,
     DrawerHeaderModule,
     MarkdownModule,
-    SharedModule
+    SharedModule,
+    CarouselModule
   ],
   exports: [BlogComponent]
 })
