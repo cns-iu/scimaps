858193a3386251f2dc914f7636a633c605623967
swapnil <ddevil691@gmail.com>
2021-11-29 02:08:50 -0500

    added filter logic on publish date

--- website/projects/scimaps/src/app/pages/blogs/blogs-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/blogs/blogs-resolver.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Params, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { ContentService } from '../../shared/services/content.service';
 import { toBlog } from './blog-resolver.service';
 
@@ -12,6 +12,7 @@ export interface Blog {
   body: string;
   slug: string;
   published: boolean;
+  publish_date: string;
   blogImages: { sm: string, lg: string }[];
 }
 
@@ -28,6 +29,16 @@ export class BlogsResolverService implements Resolve<Blog[]> {
     return this.contentService.getIndex<Params>('app-blogs').pipe(
       map((items: Params[]) => {
         return items.map((item: Params) => toBlog(item, this.directory));
+      }),
+      map((items: Blog[]) => {
+        return items.filter((item: Blog) => {
+          const today = new Date();
+          const todayUTC = Date.parse(today.toUTCString());
+          const publishedDate = new Date(item.publish_date);
+          publishedDate.setUTCHours(0, 0, 0, 0);
+          const publishDateUTC = Date.parse(publishedDate.toUTCString());
+          return todayUTC > publishDateUTC;
+        });
       })
     );
   }
