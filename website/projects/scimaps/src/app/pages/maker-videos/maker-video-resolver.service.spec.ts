82986b8187e7d5867caf2157208fbbc77935e235
swapnil <ddevil691@gmail.com>
2021-11-30 15:41:59 -0500

    added httpclient in spec

--- website/projects/scimaps/src/app/pages/maker-videos/maker-video-resolver.service.spec.ts
+++ website/projects/scimaps/src/app/pages/maker-videos/maker-video-resolver.service.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { ContentService } from '../../shared/services/content.service';
 
 import { MakerVideoResolverService } from './maker-video-resolver.service';
 
 describe('MakerVideoResolverService', () => {
   let service: MakerVideoResolverService;
-
+  let contentService: ContentService;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const contentServiceSpy = jasmine.createSpyObj('ContentService', [
+      'getContent',
+    ]);
+    TestBed.configureTestingModule({
+      providers: [
+        MakerVideoResolverService,
+        { provide: ContentService, useValue: contentServiceSpy },
+      ]
+    });
     service = TestBed.inject(MakerVideoResolverService);
+    contentService = TestBed.inject(ContentService);
   });
 
   it('should be created', () => {
