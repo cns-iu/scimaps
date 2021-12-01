9b88601f9207b1c23611d33b6eeac743633a0c82
swapnil <ddevil691@gmail.com>
2021-12-01 02:56:34 -0500

    spec fixes

--- website/projects/scimaps/src/app/pages/maker-videos/maker-videos-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/maker-videos/maker-videos-resolver.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Params, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
-import { isHttp } from '../../constants/utils';
-import { ContentService, toSlug } from '../../shared/services/content.service';
+import { map } from 'rxjs/operators';
+import { ContentService } from '../../shared/services/content.service';
 import { toMakerVideo } from './maker-video-resolver.service';
 
 export interface MakerVideo {
@@ -23,7 +22,7 @@ export const getMakerVideo = (n: number): MakerVideo[] => {
       title: `title${i}`,
       short_description: `short ${i}`,
       long_description: `long ${i}`,
-      slug: `slug${i}`,
+      slug: `title${i}`,
       videoLink: `link$ ${i}`,
       maker: 'maker/readme',
       image: `image.${i}.jpg`
