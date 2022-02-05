f69e1b5d34d013e8ab46570f196f589465c4705d
swapnil <ddevil691@gmail.com>
2022-02-04 19:46:41 -0500

    route config

--- website/projects/scimaps/src/app/pages/macroscope/macroscope-routing.module.ts
+++ website/projects/scimaps/src/app/pages/macroscope/macroscope-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { of } from 'rxjs';
 import { MapMacroscopeResolverService } from '../../shared/services/map-macroscope-resolver.service';
+import { MacroscopeDetailComponent } from './macroscope-detail/macroscope-detail.component';
 import { MacroscopeComponent } from './macroscope.component';
+
 
 
 const routes: Routes = [{
@@ -11,7 +13,18 @@ const routes: Routes = [{
   resolve: {
     macroscope: MapMacroscopeResolverService
   },
-  runGuardsAndResolvers: 'paramsOrQueryParamsChange'
+  runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+  children: [
+    {
+      path: 'detail',
+      data: {
+        video: of({
+          videoLink: 'http://idemo.cns.iu.edu/macroscopes/fleetmon.webm'
+        })
+      },
+      component: MacroscopeDetailComponent,
+    },
+  ],
 }];
 
 @NgModule({
--- website/projects/scimaps/src/app/pages/macroscope/macroscope.module.ts
+++ website/projects/scimaps/src/app/pages/macroscope/macroscope.module.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MacroscopeRoutingModule } from './macroscope-routing.module';
 import { MacroscopeComponent } from './macroscope.component';
 import { ItemDrawerModule } from '../../shared/components/item-drawer/item-drawer.module';
+import { MakerVideoModule } from '../../shared/components/maker-video/maker-video.module';
+import { MacroscopeDetailComponent } from './macroscope-detail/macroscope-detail.component';
+import { DrawerModule } from '../../shared/components/drawer/drawer.module';
 
 @NgModule({
-  declarations: [MacroscopeComponent],
+  declarations: [MacroscopeComponent, MacroscopeDetailComponent],
   imports: [
     CommonModule,
+    MakerVideoModule,
     MacroscopeRoutingModule,
-    ItemDrawerModule
+    ItemDrawerModule,
+    DrawerModule
   ]
 })
 export class MacroscopeModule { }
