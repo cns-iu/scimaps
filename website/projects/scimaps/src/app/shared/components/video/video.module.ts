384d5030129d5060089dc2ba8ba82b89202a6e6f
swapnil <ddevil691@gmail.com>
2022-02-09 02:18:11 -0500

    added video module

---
+++ website/projects/scimaps/src/app/shared/components/video/video.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoComponent } from './video.component';
+
+
+
+@NgModule({
+  declarations: [VideoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VideoModule { }
