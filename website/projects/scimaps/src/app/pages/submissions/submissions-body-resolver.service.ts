52db3e0efc4cda36f1d0b51f56156cd20415bb60
swapnil <ddevil691@gmail.com>
2022-02-28 11:59:00 -0500

    added site body page

--- website/projects/scimaps/src/app/pages/submissions/submissions-body-resolver.service.ts
+++ website/projects/scimaps/src/app/pages/submissions/submissions-body-resolver.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { ContentService } from '../../shared/services/content.service';
 
-interface SubmissionBody {
+interface SubmissionsBody {
   body: string;
 }
 @Injectable({
   providedIn: 'root'
 })
-export class SubmissionsBodyResolverService implements Resolve<SubmissionBody> {
-
-  constructor() { }
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SubmissionBody> | Observable<never> {
-    return of({
-      body: 'sample submission body'
-    });  
+export class SubmissionsBodyResolverService implements Resolve<SubmissionsBody> {
+  directory = 'assets/content/site/submissions';
+  
+  constructor(private content: ContentService, private router: Router) { }
+  
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<SubmissionsBody> | Observable<never> {
+    return this.content.getContent<SubmissionsBody>('site/submissions/submissions.md').pipe(
+      take(1)
+    );  
   }
 }
