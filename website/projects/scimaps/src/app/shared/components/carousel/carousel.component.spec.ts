cf18be6ffd9774aba590603d7f69de1cc3de98ca
swapnil <ddevil691@gmail.com>
2022-05-11 06:35:30 -0400

    removed code smell

--- website/projects/scimaps/src/app/shared/components/carousel/carousel.component.spec.ts
+++ website/projects/scimaps/src/app/shared/components/carousel/carousel.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SwiperDirective } from 'ngx-swiper-wrapper';
-import { Shallow } from 'shallow-render';
 import { CarouselComponent } from './carousel.component';
 import { CarouselModule } from './carousel.module';
 
