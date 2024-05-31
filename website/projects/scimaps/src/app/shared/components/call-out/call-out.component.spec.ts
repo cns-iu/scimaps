c7c6654c92a0cbe301ee5d47daf6f0179eed00b5
bhushankhope <bhushankhope@gmail.com>
2024-05-31 14:09:01 -0700

    fix linting

--- website/projects/scimaps/src/app/shared/components/call-out/call-out.component.spec.ts
+++ website/projects/scimaps/src/app/shared/components/call-out/call-out.component.spec.ts
@@ -4,9 +4,9 @@ import { CallOutComponent } from './call-out.component';
 import { CallOutModule } from './call-out.module';
 
 const testItems: Array<[string, Date]> = [
-  ['test', new Date],
-  ['test', new Date],
-  ['test', new Date]
+  ['test', new Date()],
+  ['test', new Date()],
+  ['test', new Date()],
 ];
 
 describe('CallOutComponent', () => {
@@ -17,13 +17,17 @@ describe('CallOutComponent', () => {
   });
 
   it('should create an item for each date passed in', async () => {
-    const { find } = await shallow.render({ bind: { items: testItems, title: 'test' }});
+    const { find } = await shallow.render({
+      bind: { items: testItems, title: 'test' },
+    });
     const items = find('.item');
     expect(items.length).toEqual(testItems.length);
   });
 
   it('should use the title passed in correctly', async () => {
-    const { find } = await shallow.render({ bind: { items: testItems, title: 'test-title' }});
+    const { find } = await shallow.render({
+      bind: { items: testItems, title: 'test-title' },
+    });
     const title = find('.title')?.nativeElement as Element;
     expect(title?.innerHTML).toContain('test-title');
   });
