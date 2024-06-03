e3d426e019ab575473f252db2b7786bd79337962
Bhushan Khope <53601863+bhushankhope@users.noreply.github.com>
2024-06-03 09:38:13 -0700

    add strikethrough logic for dates (#266)
    * add strikethrough logic for dates
    
    * fix test
    
    * fix linting
    
    * add test to check date
    
    * fix linting
    
    * add new test
    
    * fix call-out test

--- website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes.component.ts
+++ website/projects/scimaps/src/app/pages/call-for-macroscopes/call-for-macroscopes.component.ts
@@ -1,40 +1,44 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { drawerInOut } from '../../constants/drawer.animations';
-import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'sci-call-for-macroscopes',
   templateUrl: './call-for-macroscopes.component.html',
   styleUrls: ['./call-for-macroscopes.component.scss'],
-  animations: [
-    drawerInOut
-  ]
+  animations: [drawerInOut],
 })
 export class CallForMacroscopesComponent implements OnInit {
   @HostBinding('class') readonly clsName = 'sci-call-for-macrosopes';
   tabHeaders: string[] = [];
   tabContents: string[] = [];
-  lastIterationThumbnails: {image: string, title: string}[] = [];
+  lastIterationThumbnails: { image: string; title: string }[] = [];
   activePageTab = 0;
   showDrawer = false;
   lastIteration = 16;
-  importantDates: Array<[string, string]> = [];
+  importantDates: Array<[string, Date]> = [];
 
   submitURL = '';
   pdfLink = '';
   allowSubmission = true;
-  constructor(private route: ActivatedRoute, private datePipe: DatePipe) {
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.data.subscribe((data: Params) => {
       const { body } = data;
       // Body
       if (body) {
-        const { importantDates, pdfLink, lastIteration, submitURL, tabs, allowSubmission } = body;
+        const {
+          importantDates,
+          pdfLink,
+          lastIteration,
+          submitURL,
+          tabs,
+          allowSubmission,
+        } = body;
         // Tabs
         if (tabs && Array.isArray(tabs)) {
-          tabs.forEach((tab: {header: string, content: string}) => {
+          tabs.forEach((tab: { header: string; content: string }) => {
             this.tabHeaders.push(tab.header);
             this.tabContents.push(tab.content);
           });
@@ -44,9 +48,11 @@ export class CallForMacroscopesComponent implements OnInit {
         this.submitURL = submitURL;
         this.pdfLink = pdfLink;
         this.allowSubmission = allowSubmission;
-        this.importantDates = importantDates.map(((item: {label: string, date: Date}) => {
-          return [item.label, this.datePipe.transform(item.date, 'MMM dd, yyyy', '+0000')];
-        }));
+        this.importantDates = importantDates.map(
+          (item: { label: string; date: Date }) => {
+            return [item.label, item.date];
+          }
+        );
       }
       // Last Macroscope Iteraction
       const { macroscopes } = data;
