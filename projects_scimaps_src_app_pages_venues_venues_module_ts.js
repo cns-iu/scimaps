b39d1fed252d3ede682c683abd428baf4d934d36
bhushankhope <bhushankhope@users.noreply.github.com>
2024-08-01 19:50:25 +0000

    Production deploy -- 49bb8d5d449486c521494f1d84db71e6bbe4b85e

--- projects_scimaps_src_app_pages_venues_venues_module_ts.js
+++ projects_scimaps_src_app_pages_venues_venues_module_ts.js
@@ -1413,7 +1413,6 @@ class VenuesComponent {
             const { venues, body } = data;
             this.body = body;
             if (venues && Array.isArray(venues)) {
-                console.log(venues);
                 this.dataSource.data = venues;
                 // Assign predicate
                 this.dataSource.filterPredicate = this.filterData;
